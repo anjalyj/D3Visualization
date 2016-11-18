3cf3fc26b32c94221b058c0f9b7985da8c1c68cc
anjalyj <anjalyj@thoughtworks.com>
2016-11-18 16:50:42 +0530

    Anjaly | created graph using an equation.

---
+++ SineGraph/public/javascript/sineGraph.js
@@ -0,0 +1,74 @@
+var points = [{x:0, y:5},{x:1, y:9},{x:2, y:7},{x:3, y:5},{x:4, y:3},{x:6, y:4},{x:7, y:2},{x:8, y:3},{x:9, y:2}];
+
+var sinePoints = [0,1,2,3,4,5,6,7,8,9];
+
+const WIDTH = 800;
+const HEIGHT = 800;
+const MARGIN = 30;
+
+const INNER_WIDTH = WIDTH - 2 * MARGIN;
+const INNER_HEIGHT = HEIGHT - 2 * MARGIN;
+
+var svg, xScale, yScale;
+
+var translate = function(x, y){
+	return "translate("+x+","+y+")";
+};
+
+var loadChart = function(){
+	svg = d3.select('.container').append('svg')
+		.attr('width', WIDTH)
+		.attr('height', HEIGHT);
+
+	xScale = d3.scaleLinear()
+		.domain([0, 10])
+		.range([0, INNER_WIDTH]);
+
+	yScale = d3.scaleLinear()
+		.domain([0, 1])
+		.range([INNER_HEIGHT, 0]);
+
+	var xAxis = d3.axisBottom(xScale);
+	var yAxis = d3.axisLeft(yScale);
+
+	svg.append('g')
+		.attr('transform', translate(MARGIN, HEIGHT - MARGIN))
+		.call(xAxis)
+		.classed('xAxis', true);
+
+	svg.append('g')
+		.attr('transform', translate(MARGIN, MARGIN))
+		.classed('yAxis', true)
+		.call(yAxis);
+
+	createSineGraph();
+}
+
+var createSineGraph = function(){
+	var sineTransform = svg.append('g')
+		.attr('transform',  translate(MARGIN, MARGIN));
+
+	var sinePath = sineTransform.append("path")
+				.classed("line",true);
+
+	var sineLine = d3.line()
+			.x(function(d,i) { return xScale(i); })
+			.y(function(d,i) { return yScale((Math.sin(3 * d)+1)/2); })
+
+
+
+	sinePath.attr("d",sineLine(sinePoints))
+
+	sineTransform.selectAll('circle').data(sinePoints)
+		.enter().append('circle')
+		.classed("circles",true)
+		.attr('r', 8);
+
+	var circles = sineTransform.selectAll('circle');
+	
+	circles.attr('cx', function(q,i){return xScale(i)})
+		.attr('cy', function(q,i){return yScale((Math.sin(3 * q)+1)/2);});
+		
+}
+
+window.onload = loadChart; 
