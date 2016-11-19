0d65e3f8a1c230705e5e4948e4d0baffabbea239
anjalyj <anjalyj@thoughtworks.com>
2016-11-19 11:38:17 +0530

    Anjaly | created multiple pie charts and donut charts.

---
+++ PieCharts/public/javascript/pieChart.js
@@ -0,0 +1,39 @@
+var data = [1, 1, 2, 2, 1, 2, 1];
+
+const WIDTH = 800;
+const HEIGHT = 800;
+var radius = Math.min(WIDTH, HEIGHT) / 2;
+
+var translate = function(x, y){
+	return "translate("+x+","+y+")";
+};
+
+
+var loadChart = function(){
+	svg = d3.select('.container').append('svg')
+		.attr('width', WIDTH)
+		.attr('height', HEIGHT)
+		.append('g')
+    	.attr("transform", translate(WIDTH/2, HEIGHT/2));
+
+    var arc = d3.arc()
+	    .outerRadius(radius)
+	    .innerRadius(0)
+
+	var pie = d3.pie()
+    	.sort(null)
+    	.value(function(d) { return d });
+
+    var g = svg.selectAll("g")
+	   .data(pie(data))
+	   .enter().append("g");
+
+	var color = d3.schemeCategory20;
+
+	g.append("path")
+	    .attr("d", arc)
+	    .style("fill", function(d,i) { return color[i]; });
+
+}
+
+window.onload = loadChart;
